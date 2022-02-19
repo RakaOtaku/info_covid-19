7fc57243a226f997e4a665606df756513cca924c
FIRST_NAME LAST_NAME <xhuagf299@gmail.com>
2022-02-20 01:09:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/infocovid_19/model/IndonesiaResponse.kt
@@ -0,0 +1,9 @@
+package com.example.infocovid_19.model
+
+data class IndonesiaResponse(
+    val attributes: String,
+    val positif: String,
+    val sembuh: String,
+    val dirawat: String,
+    val meninggal: String
+    )
